360b2566c1de5b5857cb0653911a134b6edbb06e
MiQannn <ititiu18271@student.hcmiu.edu.vn>
2022-10-09 19:35:33 +0700

    new NFT-Collection

---
+++ nft/hardhat/constants/index.js
@@ -0,0 +1,6 @@
+// Address of the Whitelist Contract that you deployed
+const WHITELIST_CONTRACT_ADDRESS = "0x3cA8A46beD6B2845037ff52Ad66b4558047f599A";
+// URL to extract Metadata for a Crypto Dev NFT
+const METADATA_URL = "https://thesis-kfky5p96u-miqannn.vercel.app/api/";
+
+module.exports = { WHITELIST_CONTRACT_ADDRESS, METADATA_URL };
